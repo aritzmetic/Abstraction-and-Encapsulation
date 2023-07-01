ceb958a3f686fffb6cc0662ec0284cbded62b50b
unknown <arixxdelera21@gmail.com>
2023-07-02 07:40:14 +0800

    constructor has been added

--- The Fan Class/fan-class.py
+++ The Fan Class/fan-class.py
@@ -1,7 +1,15 @@
 # Delera, Aritz B.
 
 # (The Fan class).  Design a class named Fan to represent a fan. The class contains:
+class Fan:
 # ■ Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+    SLOW = 1
+    MEDUIM = 2
+    FAST = 3
+
+    # ■ A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
+    def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
+        pass
 # ■ A private int data field named speed that specifies the speed of the fan.
 # ■ A private bool data field named on that specifies whether the fan is on (the default is False).
 # ■ A private float data field named radius that specifies the radius of the fan.
