c6818feeb7db63d934518381f14b832e4f81b18b
unknown <arixxdelera21@gmail.com>
2023-07-02 13:51:45 +0800

    class testpetand user prompt has been created

--- The Pet Class/pet.py
+++ The Pet Class/pet.py
@@ -30,4 +30,3 @@ class Pet:
 # • get_age() This method returns the value of the _ _age field.
     def get_age(self):
         return self.__age
-# Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. This data should be stored as the object’s attributes. Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
--- The Pet Class/testPet.py
+++ The Pet Class/testPet.py
@@ -1,8 +1,16 @@
 # Delera, Aritz B.
 
 # Import pet class
+from pet import Pet
 # Once you have written the class, 
 # write a program that creates an object of the class and 
-# prompts the user to enter the name, type, and age of his or her pet. 
+class TestPet:
+    def __init__(self):
+        self.user_pet = Pet()
+    # prompts the user to enter the name, type, and age of his or her pet. 
+    def user_prompt(self):
+        name = input("What is the name of ypur pet?: ")
+        animal_type = input("What type of animal is he/she?(cat/dog/others): ")
+        age = int(input("What is his/her age?: "))
 # This data should be stored as the object’s attributes. 
 # Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
