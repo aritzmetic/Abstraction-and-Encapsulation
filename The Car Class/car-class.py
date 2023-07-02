349a5f8486212f6fce952de71b7ce6247da411eb
unknown <arixxdelera21@gmail.com>
2023-07-02 16:08:08 +0800

    methods has been finished

--- The Car Class/car-class.py
+++ The Car Class/car-class.py
@@ -14,10 +14,17 @@ class Car:
         self.__make = make
         self.__speed = 0
 
-# The class should also have the following methods:
-# • accelerate() The accelerate method should add 5 to the speed data attribute each time it is called.
-# • brake() The brake method should subtract 5 from the speed data attribute each time it is called.
-# • get_speed() The get_speed method should return the current speed.
+    # The class should also have the following methods:
+    # • accelerate() The accelerate method should add 5 to the speed data attribute each time it is called.
+    def accelerate(self):
+        self.__speed += 5
+    # • brake() The brake method should subtract 5 from the speed data attribute each time it is called.
+    def brake(self):
+        self.__speed -= 5
+    # • get_speed() The get_speed method should return the current speed.
+    def get_speed(self):
+        return self.__speed
+
 
 # Next, design a program that creates a Car object then calls the accelerate method five times. 
 # After each call to the accelerate method, get the current speed of the car and display it. 
