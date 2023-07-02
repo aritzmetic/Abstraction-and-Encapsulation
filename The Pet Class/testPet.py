7b2d1fe229e2aff3fdc6e520fae308e770ccb95a
unknown <arixxdelera21@gmail.com>
2023-07-02 14:09:02 +0800

    the output has been displayed

--- The Pet Class/testPet.py
+++ The Pet Class/testPet.py
@@ -12,5 +12,20 @@ class TestPet:
         name = input("What is the name of ypur pet?: ")
         animal_type = input("What type of animal is he/she?(cat/dog/others): ")
         age = int(input("What is his/her age?: "))
-# This data should be stored as the object’s attributes. 
-# Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+
+        # This data should be stored as the object’s attributes. 
+        self.user_pet.set_name(name)
+        self.user_pet.set_animal_type(animal_type)
+        self.user_pet.set_age(age)
+
+    # Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+    def display_pet_information(self):
+        print("Here is the information of your Pet!")
+        print("NAME:", self.user_pet.get_name())
+        print("ANIMAL TYPE:", self.user_pet.get_animal_type())
+        print("AGE:", self.user_pet.get_age())
+
+
+test_pet = TestPet()
+test_pet.user_prompt()
+test_pet.display_pet_information()
