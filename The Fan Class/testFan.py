e3dfcebfced58a5f1b71950fe2987b68846260d5
unknown <arixxdelera21@gmail.com>
2023-07-02 08:22:59 +0800

    seperate file for test fan has been added; psuedocode has also been movcd

---
+++ The Fan Class/testFan.py
@@ -0,0 +1,14 @@
+# Delera, Aritz B.
+
+# For the first object, 
+# assign the maximum speed, 
+# radius 10, 
+# color yellow, and 
+# turn it on. 
+
+# for the second object,
+# Assign medium speed, 
+# radius 5, 
+# color blue, and 
+# turn it off 
+# Display each object’s speed, radius, color, and on properties.
