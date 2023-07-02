f95b7eb0da640b9962469e4235632d4b15b8bc87
unknown <arixxdelera21@gmail.com>
2023-07-02 16:22:14 +0800

    class has been added

--- The Car Class/testCar.py
+++ The Car Class/testCar.py
@@ -1,5 +1,14 @@
 # Delera, Aritz B.
 
+# Import the car class
+from car import Car
+
+# make a class testCar
+class testCar:
+    pass
+
+# Allow the user to input for the card object.
+
 # Next, design a program that creates a Car object then calls the accelerate method five times. 
 # After each call to the accelerate method, get the current speed of the car and display it. 
 # Then call the brake method five times. 
