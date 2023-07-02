8365228262fc8212a9eb4dea1bcdb69ac0f9b6f5
unknown <arixxdelera21@gmail.com>
2023-07-02 16:03:11 +0800

    class car has been added and its attributes by which year model and make is set as an argument

--- The Car Class/car-class.py
+++ The Car Class/car-class.py
@@ -1,14 +1,18 @@
 # Delera, Aritz B.
 
 # A. Car Class
-# Write a class named Car that has the following data attributes:
-# • _ _year_model (for the car’s year model)
-# • _ _make (for the make of the car)
-# • _ _speed (for the car’s current speed)
-
-# The Car class should have an _ _init_ _ method that accepts the car’s year model and make as arguments. 
-# These values should be assigned to the object’s _ _year_model and _ _make data attributes. 
-# It should also assign 0 to the _ _speed data attribute.
+class Car:
+    # Write a class named Car that has the following data attributes:
+    # • _ _year_model (for the car’s year model)
+    # • _ _make (for the make of the car)
+    # • _ _speed (for the car’s current speed)
+    # The Car class should have an _ _init_ _ method that accepts the car’s year model and make as arguments. 
+    # These values should be assigned to the object’s _ _year_model and _ _make data attributes. 
+    # It should also assign 0 to the _ _speed data attribute.
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
 
 # The class should also have the following methods:
 # • accelerate() The accelerate method should add 5 to the speed data attribute each time it is called.
