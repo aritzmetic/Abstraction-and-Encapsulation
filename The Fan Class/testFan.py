69832be7f7141d1c0c3b9678c283981a005bf426
unknown <arixxdelera21@gmail.com>
2023-07-02 10:18:36 +0800

    objects has been added

--- The Fan Class/testFan.py
+++ The Fan Class/testFan.py
@@ -3,16 +3,22 @@
 from fan import Fan
 
 # create a testfan class
+class TestFan:
+    def testrun(self):
+        # For the first object, 
+        # assign the maximum speed, 
+        # radius 10, 
+        # color yellow, and  
+        # turn it on. 
+        fan1 = Fan(speed=Fan.FAST, radius=10, color="yellow", on=True)
 
-# For the first object, 
-# assign the maximum speed, 
-# radius 10, 
-# color yellow, and  
-# turn it on. 
+        # for the second object,
+        # Assign medium speed, 
+        # radius 5, 
+        # color blue, and 
+        # turn it off 
+        # Display each object’s speed, radius, color, and on properties.
+        fan2 = Fan(speed=Fan.MEDIUM, radius=5, color="blue", on=False)
 
-# for the second object,
-# Assign medium speed, 
-# radius 5, 
-# color blue, and 
-# turn it off 
-# Display each object’s speed, radius, color, and on properties.
+        
+
